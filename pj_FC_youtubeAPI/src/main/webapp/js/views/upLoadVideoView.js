b6bd010fb93a65ddb6903c69e24ca55509fd8159
boABae <beaboa0531@naver.com>
2016-11-30 15:26:21 +0900

    upload completed

--- pj_FC_youtubeAPI/src/main/webapp/js/views/upLoadVideoView.js
+++ pj_FC_youtubeAPI/src/main/webapp/js/views/upLoadVideoView.js
@@ -13,18 +13,21 @@ var uploadVideo = Backbone.View.extend({
 		'click #temp': 'temp'
 	},
 	temp: function(){
+		console.log("hi");
 		$('.progress').show();
+		$('.progress-bar').addClass("progress-bar-striped active");
 	},
 	render: function(){
 		var template = _.template($("#uploadVideo_html").html(), {});
 		this.$el.html(template);
+		$('.during-upload').hide();
 		$('.progress').hide();
 	},
+	
 	videoUploadReady: function(){
 		var self = this;
-		console.log("upload");
 		$("#submit").on('click', function(){
-			console.log('temp');
+			
 			var file = $('#file').get(0).files[0];
 			if(file){
 				var metadata = {
@@ -52,26 +55,81 @@ var uploadVideo = Backbone.View.extend({
 						start: 0
 					});
 				});
+			}else{
+				alert("file을 선택해주세요");
 			}
 		});
 	},
 	resumableUpload: function(options){
-		console.log(options.url);
-		$.ajax({
-			url: options.url,
-			method: 'PUT',
-			contentType: options.file.type,
-		    headers: {
+		var self = this;
+		var ajax = $.ajax({
+		      url: options.url,
+		      method: 'PUT',
+		      contentType: options.file.type,
+		      headers: {
 		        'Content-Range': 'bytes ' + options.start + '-' + (options.file.size - 1) + '/' + options.file.size
-		    },
-		    xhr: function(){
-		    	var xhr = $.ajaxSettins.xhr();
-		    	$('.progress').show();
-		    },
-		    processData: false,
-		    data: options.file
+		      },
+		      xhr: function() {
+		        var xhr = $.ajaxSettings.xhr();
+		        if (xhr.upload) {
+		        	xhr.upload.addEventListener('progress', function(e){
+		        		if(e.lengthComputable){
+		        			var bytesTransferred = e.loaded;
+			                var totalBytes = e.total;
+			                var percentage = Math.round(100 * bytesTransferred / totalBytes);
+			                $('.progress').show();
+			                $(".progress-bar").attr({
+			                	'style': 'width :' + percentage + '%',
+			                });
+		        		}
+		        		
+		        	},
+		        	false
+		        	);
+		        }
+		        return xhr;
+		      },
+		      processData: false,
+		      data: options.file
+		    });
+
+		    ajax.done(function(response) {
+		    	$("#uploading").html("업로딩중 잠시만 기다려 주세요!");
+		    	$('.progress-bar').addClass("progress-bar-striped active");
+		    	videoId = response.id;
+		    	self.checkVideoStatus(videoId, INITIAL_STATUS_POLLING_INTERVAL_MS);
+		    });
+	},
+	checkVideoStatus: function(videoId, waitFornextPoll){
+		var self = this;
+		$.ajax({
+			url: VIDEOS_SERVICE_URL,
+		      method: 'GET',
+		      headers: {
+		        Authorization: 'Bearer ' + accessToken
+		      },
+		      data: {
+		        part: 'status,processingDetails,player',
+		        id: videoId
+		      }
 		}).done(function(response){
-			console.log(response);
+			var uploadStatus = response.items[0].status.uploadStatus;
+			var embed = response.items[0].player.embedHtml;
+			console.log(embed);
+			console.log(uploadStatus);
+			if(uploadStatus == 'uploaded'){
+				setTimeout(function(){
+					self.checkVideoStatus(videoId, waitFornextPoll * 2);
+				}, waitFornextPoll);
+			}else{
+				if(uploadStatus == 'processed'){
+					console.log("finally completed!");
+					$("#uploading").hide();
+					$(".progress-bar").removeClass("progress-bar-striped active");
+					$('.container').find('.embed-responsive').append(embed);
+					
+				}
+			}
 		});
-	}
+	},
 });
